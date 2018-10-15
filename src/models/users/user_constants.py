e2456d5246ea7464962c193eb4a9cf5ab1509cf1
Kremzeeq <41695967+Kremzeeq@users.noreply.github.com>
2018-10-15 09:31:38 +0100

    User constants updated

---
+++ src/models/users/user_constants.py
@@ -0,0 +1,9 @@
+COLLECTION = "Users"
+
+URL = "https://api.mailgun.net/v3/sandboxxxxxxxxxxxxxxxxxxxxxxxxx.mailgun.org/messages"
+
+AUTH_URL = "http://127.0.0.1:4996/users/auth"
+
+API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+FROM = "Mailgun sandbox <postmaster@sandboxxxxxxxxxxxxxxxxxxxxxxx.mailgun.org>"
